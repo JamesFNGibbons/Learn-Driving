c5bea1a2cfcd614675a9b52e8cfe74d96d977877
James Gibbons <jgibbons@jgibbons-imac.local>
2017-10-09 22:54:29 +0100

    Added add user dialog*

--- src/AccountManager.java
+++ src/AccountManager.java
@@ -1,5 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
@@ -26,6 +27,90 @@ public class AccountManager {
 		this.root = app;
 	}
 	
+	/**
+	 * Function used to display the create user window.
+	 */
+	public void create_user_window(){
+		// Create and display the window.
+		JFrame window = new JFrame();
+		window.setAlwaysOnTop(true);
+		window.setVisible(true);
+		window.setTitle("Create New Account");
+		window.setResizable(false);
+		window.setSize(600, 300);
+		
+		// Create the master content pane
+		JPanel master_panel = new JPanel();
+		master_panel.setLayout(new FlowLayout());
+		
+		// Calculate the logo width based on the window size.
+		// and aspect ratio of the logo image.
+		int logo_width = window.getWidth() / 2;
+		int org_width = 413;
+		int org_height = 57;
+		int width_dif = org_width / logo_width;
+		int logo_height = org_height * width_dif;
+		
+		// Create the logo to be displayed in the side panel.
+		ImageIcon logo_icon = new ImageIcon(new ImageIcon(this.getClass().getResource("resources/logo.png")).getImage().getScaledInstance(logo_width, logo_height, Image.SCALE_SMOOTH));
+		JLabel logo = new JLabel();
+		logo.setIcon(logo_icon);
+		master_panel.add(logo);
+	
+		// Create the form panel
+		JPanel form_panel = new JPanel();
+		form_panel.setLayout(new GridLayout(7, 1));
+		form_panel.setSize(window.getWidth() / 2, window.getHeight());
+		
+		// Create the name text form
+		JLabel name_label = new JLabel("Your Full Name");
+		form_panel.add(name_label);
+		JTextField name = new JTextField();
+		form_panel.add(name);
+		
+		// Create the username form
+		JLabel username_label = new JLabel("Your Username");
+		form_panel.add(username_label);
+		JTextField username = new JTextField();
+		form_panel.add(username);
+		
+		// Create the password form
+		JLabel password_label = new JLabel("Your Password");
+		form_panel.add(password_label);
+		JPasswordField password = new JPasswordField();
+		form_panel.add(password);
+		
+		// Create an object instance for the action listener to use.
+		AccountManager root = this;
+		
+		// Create the 'Create account' buton.
+		JButton create_account = new JButton("Create Account");
+		create_account.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				root.create_account(
+					name.getText(),
+					username.getText(),
+					password.getText()
+				);
+			}
+		});
+		create_account.setSize(100, create_account.getHeight());
+		form_panel.add(create_account);
+		
+		// Add the form panel to the master panel.
+		master_panel.add(form_panel);
+		
+		// Render the window.
+		window.setContentPane(master_panel);
+		window.pack();
+		window.show();
+	}
+	
+	protected void create_account(String text, String text2, String text3) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	/**
 	 * Function used to display the incorrect login
 	 * window.
@@ -176,6 +261,13 @@ public class AccountManager {
 			public void actionPerformed(ActionEvent e) {
 				((AccountManager) root).validate_login(username.getText(), password.getText());
 				
+			}
+		});
+		
+		/* Handles the register button being clicked */
+		register.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				((AccountManager) root).create_user_window();
 			}
 		});
 	}
