a00bb2477ab6895d047ffcc9caa7817febff5568
James Gibbons <jgibbons@jgibbons-imac.local>
2017-10-09 23:31:48 +0100

    Added automated model creation

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.swing.JFrame;
 
 public class Application {
@@ -13,6 +21,7 @@ public class Application {
 		/* Load the window manager */
 		Application app = new Application();
 		app.init_application();
+		app.load_models();
 	}
 	
 	public void init_application(){
@@ -22,6 +31,21 @@ public class Application {
 		if(!this.is_loggedin){
 			AccountManager accounts = new AccountManager(this);
 			accounts.load_account_login();
+		}
+	}
+	
+	/**
+	 * Function used to loop through the models directory
+	 * and create anything new in the database.
+	 * @return
+	 */
+	public void load_models(){
+		try {
+			BufferedReader models = new BufferedReader(new FileReader(this.getClass().getClassLoader().getResource("/models/Models.sql").getFile()));
+			System.out.println(models.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
