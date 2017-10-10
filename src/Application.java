1b283937f29320d069178f4c09ba2fa491119d57
James Gibbons <jgibbons@jgibbons-imac.local>
2017-10-10 08:09:03 +0100

    Completed basic auto updating models

--- src/Application.java
+++ src/Application.java
@@ -2,9 +2,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JFrame;
 
@@ -40,12 +44,40 @@ public class Application {
 	 * @return
 	 */
 	public void load_models(){
+		String dir = new File("src").getAbsolutePath();
+		String _models = dir + "/models/Models.sql";
+		
+		String content = null;
+		
 		try {
-			BufferedReader models = new BufferedReader(new FileReader(this.getClass().getClassLoader().getResource("/models/Models.sql").getFile()));
-			System.out.println(models.toString());
-		} catch (FileNotFoundException e) {
+			content = new String(Files.readAllBytes(Paths.get(_models)));
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		// Setup the sql connection to the database
+		// and create the connection that will be used
+		// to execute the contents of the models.sql file.
+		Database db = new Database();
+		Statement statment = null;
+		
+		try {
+			statment = db.get().createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Run the contents of the sql file.
+		try {
+			statment.execute(content);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// All done. Close the connection to the database. 
+		db.close();
 	}
 }
---
+++ src/models/Models.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS Accounts(
+	ID INT AUTO_INCREMENT,
+    Name TEXT,
+    Username TEXT,
+    Password TEXT,
+    
+    PRIMARY KEY(ID)
+);
